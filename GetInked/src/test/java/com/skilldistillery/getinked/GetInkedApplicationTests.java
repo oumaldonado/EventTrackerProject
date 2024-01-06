58795d5e7262f898b28fc75905d1ccd1723eb171
Osvaldo Maldonado <osvaldomaldonado404@gmail.com>
2024-01-05 19:25:17 -0500

    REST project made

---
+++ GetInked/src/test/java/com/skilldistillery/getinked/GetInkedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.getinked;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetInkedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
