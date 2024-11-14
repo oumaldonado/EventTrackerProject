57499b69873b9fb293563312e5ca0b1338a7922b
Osvaldo Maldonado <osvaldomaldonado404@gmail.com>
2024-11-14 16:11:54 -0500

    updated UI

--- GetInked/src/test/java/com/skilldistillery/getinked/GetInkedApplicationTests.java
+++ GetInked/src/test/java/com/skilldistillery/getinked/GetInkedApplicationTests.java
@@ -1,13 +1,10 @@
 package com.skilldistillery.getinked;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GetInkedApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	
 
 }
